a9005d1115921ed20b0672a66360019aab16f356
Saeid Al-Wazzan <saeid.wazzan@nonsingular.net>
2024-07-06 23:45:35 +0200

    Update vim settings to use our vimrc

--- home.nix
+++ home.nix
@@ -18,7 +18,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    pkgs.vim
     pkgs.ripgrep
     pkgs.fzf
   ];
@@ -26,16 +25,6 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   # Home Manager can also manage your environment variables through
@@ -55,7 +44,7 @@
   #  /etc/profiles/per-user/saeidw/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    # EDITOR = "vim";
   };
 
   # Let Home Manager install and manage itself.
@@ -76,5 +65,15 @@
     enable = true;
     userName = "Saeid Al-Wazzan";
     userEmail = "saeid.wazzan@nonsingular.net";
+  };
+
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+    plugins = [
+      pkgs.vimPlugins.fzfWrapper
+      # pkgs.vimPlugins.coc-nvim
+    ];
+    extraConfig = builtins.readFile ./vim/vimrc;
   };
 }
