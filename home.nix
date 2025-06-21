dd16a4565ab95d338ee237712c8596cc4e3bb30c
Saeid Al-Wazzan <227545+saeidw@users.noreply.github.com>
2025-06-21 14:49:58 +0200

    Read user information from user.nix (ignored)

--- home.nix
+++ home.nix
@@ -1,10 +1,13 @@
 { config, pkgs, ... }:
 
+let
+  user = import ./user.nix;
+in
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "saeidw";
-  home.homeDirectory = "/home/saeidw";
+  home.username = user.username;
+  home.homeDirectory = user.homeDirectory;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -13,7 +16,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -24,6 +27,7 @@
     pkgs.timewarrior
     pkgs.taskwarrior3
     pkgs.jq
+    pkgs.tree
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -86,8 +90,8 @@
 
   programs.git = {
     enable = true;
-    userName = "Saeid Al-Wazzan";
-    userEmail = "saeid.wazzan@nonsingular.net";
+    userName = user.fullName;
+    userEmail = user.email;
   };
 
   programs.vim = {
